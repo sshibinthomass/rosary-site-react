299450228654f01356bd08c8db8d5630773ab04f
Shibin Thomas <sshibinthomass@gmail.com>
2026-07-19 17:59:36 +0200

    Fix GitHub Pages base path for reverted main (08cd682)

---
+++ assets/web-BO9sSjRN.js
@@ -0,0 +1,1 @@
+import{a8 as n}from"./index-DRCJmyos.js";import"./vendor-react--rIqVvME.js";class t extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{t as AppLauncherWeb};
